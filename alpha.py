2643ddcebfc271809bfca11956a35aed24ce968a
zandala <zandala_lkx@163.com>
2024-12-10 15:33:26 +0800

    feat：add todo list

--- alpha.py
+++ alpha.py
@@ -10,13 +10,10 @@ from logger.logger import logger
 ts = TimeSeries(key='31E212FT9F0YULWX', output_format='pandas')
 ti = TechIndicators(key='31E212FT9F0YULWX', output_format='pandas')
 
-# 获取日线数据
-# data, meta_data = ts.get_daily(symbol='AAPL', outputsize='compact')  # 输出size可选'compact'（最近100天）或'full'（所有可用数据）
-# print(data)
-
 # 创建 Session 类
 Session = sessionmaker(bind=engine)
 
+# 根据f_type查询f_symbol
 def get_symbols_by_type(symbol_type):
     try:
         # 创建会话
@@ -33,6 +30,25 @@ def get_symbols_by_type(symbol_type):
         logger.info(f"查询失败: {e}")
         return []
 
+# todo 每天定时访问API,获取昨天的数据，插入数据库
+
+
+def get_daily_data():
+    symbol_list = get_symbols_by_type(1)
+    for symbol in symbol_list:
+        try:
+            # 获取数据
+            data, meta_data = ts.get_daily(symbol=symbol, outputsize='compact')
+            logger.info(data)
+            # todo 获取的数据是否有问题，做一个数据清洗. 例如：如果获取的数据为空，缺失值处理，异常值处理等
+            # todo 将数据插入数据库
+            session = Session()
+
+
+
+
+        except Exception as e:
+            logger.info(f"获取数据失败: {e}")
 
 logger.info(get_symbols_by_type(1))
 
